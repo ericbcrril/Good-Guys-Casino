dfabaeb39f3fe9cbeafe36b2056797d1bb7d1bda
AntitonioMoreno <129530982+AntitonioMoreno@users.noreply.github.com>
2024-06-18 10:55:42 -0600

    login added

---
+++ appweb/src/views/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MainNabvar } from "../components/navbars/MainNavbar";
+
+function Login() {
+    return(
+        <main>
+            <MainNabvar />
+            <h1>Login</h1>
+        </main>
+    );
+}
+
+export default home;
