f2ae75f3700cea79e00dc13983af9824662eee65
AntitonioMoreno <129530982+AntitonioMoreno@users.noreply.github.com>
2024-06-19 02:09:28 -0600

    desing incomplete for login

--- appweb/src/views/Login.js
+++ appweb/src/views/Login.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
 import MainNabvar from "../components/navbars/MainNavbar";
+import 'boxicons';
 
 function Login() {
-    return(
-        <main>
+    return (
+        <main className='main'>
             <MainNabvar />
-            <h1>Login</h1>
+            <section className='main-box'>
+                <div>
+                    <h1>Login</h1>
+                    <img src='/images/logos/logoGG.png' width={50}></img>
+                </div>
+                <div className='item1'>
+                    <h3>email</h3>
+                    <input type='email' required placeholder='introduzca su correo electronico'></input>
+                    <h3>contraseña</h3>
+                    <input type='password' required placeholder='introduzca contraseña'></input>
+                </div>
+                <div className='item2'>
+                    <div>
+                        <a href='#'><box-icon type='logo' name='facebook-circle'></box-icon></a>
+                        <a href='#'><box-icon type='logo' name='google'></box-icon></a>
+                    </div>
+                    <div>
+                        <h4>Iniciar sesion con</h4>
+                    </div>
+                </div>
+                <div className='item3'>
+                    <button>Iniciar sesion</button>
+                    <a href='#'><box-icon type='solid' name='user-plus'></box-icon></a>
+                </div>
+            </section>
         </main>
     );
 }
